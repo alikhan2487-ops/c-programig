da45049fb58369a8a5e112b0565144596d6a7e33
alikhan2487-ops <alikhan2487@eng.rizvi.edu.in>
2025-11-14 10:52:24 +0530

    Add files via upload

---
+++ exp 8b.c
@@ -0,0 +1,33 @@
+/*
+NAME:ALI AZMATULLAH KHAN
+UIN:251M011
+CLASS:F.E MECHANICAL
+DIV:F
+*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j, flag = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Find length
+    for (i = 0; str[i] != '\0'; i++);
+
+    // Check palindrome
+    for (j = 0; j < i / 2; j++) {
+        if (str[j] != str[i - j - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("The string is a palindrome.");
+    else
+        printf("The string is NOT a palindrome.");
+
+    return 0;
+}
