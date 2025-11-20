b47c1634377b142e04a3d3dcbcd4b722d87ef1f9
alikhan2487-ops <alikhan2487@eng.rizvi.edu.in>
2025-11-19 22:29:50 -1200

    Add files via upload

--- exp 6.c
+++ exp 6.c
@@ -6,26 +6,16 @@ DIV:F
 */
 #include <stdio.h>
 
-int isPrime(int num) {
-    if (num <= 1) return 0;
-    for (int i = 2; i <= num / 2; i++) {
-        if (num % i == 0)
-            return 0;
-    }
-    return 1;
+void counter() {
+	static int count = 0;
+	count ++;
+	printf("Count = %d\n", count);
 }
 
 int main() {
-    int lower, upper;
-    printf("Enter first number: ");
-    scanf("%d", &lower);
-    printf("Enter second number: ");
-    scanf("%d", &upper);
+	counter();
+	counter();
+	counter();
 
-    printf("Prime numbers between %d and %d are:\n", lower, upper);
-    for (int i = lower; i <= upper; i++) {
-        if (isPrime(i))
-            printf("%d ", i);
-    }
-    return 0;
+	return 0;
 }
